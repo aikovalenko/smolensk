96f58e04c92e6f4d124c61b8ac424d45b8c959b5
aikovalenko <kovalenko.ai@yandex.ru>
2017-09-22 14:07:03 +0300

    init

---
+++ docs/js/scripts.js
@@ -0,0 +1,8 @@
+$(".slider").slick({
+    dots: false,
+    arrows: true,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1
+});
---
+++ src/js/libs.js
@@ -0,0 +1,5 @@
+//= ../libs/jquery/dist/jquery.min.js
+
+//= ../libs/slick-carousel/slick/slick.min.js
+
+//= ../libs/fastclick/lib/fastclick.js
